e671f6a00ab46922d926e07f681a203857960f3f
azizkale <azizkale@hotmail.com>
2021-12-30 10:43:29 +0100

    new roe can be added now

--- TrenYolPriceCalculating/InsertNewRowToExcelFile.cs
+++ TrenYolPriceCalculating/InsertNewRowToExcelFile.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data;
 using System.Data.OleDb;
 using System.Windows.Forms;
 
@@ -12,20 +11,6 @@ namespace TrenYolPriceCalculating
 
        public void insertNewRow(Product p)
         {
-            //DataTable dt = new DataTable();
-            //DataGridView dgv = new DataGridView();
-            //ExcelFileColumns columns = new ExcelFileColumns();
-
-            //readEx.LoadExcelFromPC(dgv); // it runs the the function "LoadExcelFromPC" and ReadingExcel.dtExcel is able to be filled
-
-            //dt = ReadingExcel.dtExcel;
-
-            //int rowCountInTheAvailableExcelFile = 0 +1; // column headers row and another rows
-            //rowCountInTheAvailableExcelFile = dt.Rows.Count;      
-
-          
-
-
             string DocumentsAndSettingsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             string filePath = DocumentsAndSettingsPath + "\\Ürünler.xls";
             string fileExt = string.Empty;
@@ -38,7 +23,7 @@ namespace TrenYolPriceCalculating
                 MyConnection = new OleDbConnection("provider=Microsoft.Jet.OLEDB.4.0;Data Source="+ filePath + ";Extended Properties=Excel 8.0;");
                 MyConnection.Open();
                 myCommand.Connection = MyConnection;
-                sql = "Insert into [Sayfa1$] (ID,Urun_Adi) values('7','asdgfdgdfd')";
+                sql = "Insert into [Sayfa1$] (ID,Urun_Adi,KDV_Orani) values('7','asdgfdgdfd','2')";
                 myCommand.CommandText = sql;
                 myCommand.ExecuteNonQuery();
                 MyConnection.Close();
