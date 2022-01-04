46fd69a50e7d5e43e902a0d80966b01ce65962d9
azizkale <azizkale@hotmail.com>
2022-01-04 05:58:33 +0100

    now products have numbers

--- TrenYolPriceCalculating/CurrentExcelFile.cs
+++ TrenYolPriceCalculating/CurrentExcelFile.cs
@@ -170,7 +170,23 @@ namespace TrenYolPriceCalculating
            
         }
 
-       
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+           
+            var grid = sender as DataGridView;
+            var rowIdx = (e.RowIndex + 1).ToString();
+
+            var centerFormat = new StringFormat()
+            {
+                // right alignment might actually make more sense for numbers
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+
+            var headerBounds = new Rectangle(e.RowBounds.Left, e.RowBounds.Top, grid.RowHeadersWidth, e.RowBounds.Height);
+            e.Graphics.DrawString(rowIdx, new Font("Times Roman", 18, FontStyle.Regular), SystemBrushes.ControlText, headerBounds, centerFormat);
+            
+        }
     }
     
 }
